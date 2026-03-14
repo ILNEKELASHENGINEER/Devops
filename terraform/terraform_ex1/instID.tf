4d0e457307301c4a6be605f53e8e004da5b192f4
neke <nekelashil.21cse@kongu.edu>
2026-03-14 23:55:52 +0530

    terraform practice getting instance id

---
+++ terraform/terraform_ex1/instID.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ami_id" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+output "instance_ami_id" {
+  description = "AMI ID OF UBUNTU 22 JAMMY-JELLY"
+  value       = data.aws_ami.ami_id.id
+
+}
